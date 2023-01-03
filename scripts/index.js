46099047577020d6271c876ba9f2f3f08e863805
gusgluna <gusgluna@gmail.com>
2023-01-02 21:23:37 -0600

    Logica de JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,74 @@
-console.log("hola mundo");
-
 let encryptButton = document.getElementById("encryptBtn");
-
+let decryptButton = document.getElementById("decryptBtn");
+let copyButton = document.getElementById("copyBtn");
+let messageBox = document.getElementById("msgBox");
 let encryptText = document.getElementById("encryptText");
+
+function onlyLetters(text) {
+  var letters = /^[a-zA-Z\s]+$/;
+  if (text.match(letters)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function encryptTextFn(text) {
+  let arrText = text.split("");
+  let result = "";
+  arrText.forEach((element) => {
+    if (element == "e") {
+      result += "enter";
+    } else if (element == "i") {
+      result += "imes";
+    } else if (element == "a") {
+      result += "ai";
+    } else if (element == "o") {
+      result += "over";
+    } else if (element == "u") {
+      result += "ufat";
+    } else {
+      result += element;
+    }
+  });
+  return result;
+}
+
+function decryptTextFunction(text) {
+  let result = text
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/over/g, "o")
+    .replace(/ufat/g, "u");
+  return result;
+}
 
 encryptButton.addEventListener("click", function (event) {
   event.preventDefault();
-  let textValue = encryptText.value;
-  console.log(textValue);
+  if (!onlyLetters(encryptText.value)) {
+    messageBox.innerText = "Solo puedes usar letras minusculas [a-z]";
+    return;
+  }
+  let encryptedMesagge = encryptTextFn(encryptText.value.toLowerCase());
+  messageBox.innerText = encryptedMesagge;
   encryptText.value = "";
+});
+
+decryptButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (!onlyLetters(encryptText.value)) {
+    messageBox.innerText = "Solo puedes usar letras minusculas [a-z]";
+    return;
+  }
+  let decryptedMesagge = decryptTextFunction(encryptText.value);
+  messageBox.innerText = decryptedMesagge;
+  encryptText.value = "";
+});
+
+copyButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(messageBox.innerText);
+  navigator.clipboard.writeText(messageBox.innerText);
+  alert(`se copio al portapales el mensaje: ${messageBox.innerText}`);
 });
